25cc58402184edfccd2158398fcdc79d0af7d52d
amir <amir8162@gmail.com>
2022-12-04 23:47:47 +0330

    coool:)

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path 
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'hello'
 
 urlpatterns = [
-    path('', views.sessionview)
+    #path('', views.sessionview),
+    path('', TemplateView.as_view(template_name='hello/session.html')),
+    path('cookie', views.cookie),
+    path('sessfun', views.sessfun),
 ]
